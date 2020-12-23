7b13a895aa811db2f485f6b487f0af14e0b4592f
lucash-diskkun <luca5h1905@gmail.com>
2020-12-22 23:25:43 -0500

    broken don't use

--- second.asm
+++ second.asm
@@ -10,6 +10,12 @@ tile_data:
 INCBIN "tiles.chr"
 tile_data_end:
 
+SECTION "WRAM Things", WRAM0
+old_jp:
+	db
+new_jp:
+	db
+
 SECTION "game code", ROM0[$150]
 
 Start:
@@ -47,58 +53,157 @@ Start:
 	call enableLCD
 
 mainGame:
+
+testButtons:
 	ld hl, $ff00
-	res 5, [hl]
 	set 4, [hl]
+	res 5, [hl]
+	
 .loop
-	bit 0, [hl]
+	ld a, [$ff00]
+	bit	0, a
 	jr z, .a
-	bit 1, [hl]
+	bit 1, a
 	jr z, .b
-	jr nz, .loop
+	bit 3, a
+	jr z, .start
+	jr nz, testDPad
 
 .a
+	ld a, 5
+	ld [new_jp], a
 	call checkControl
 	call waitVBlank
 	ld a, [$fe02]
+	cp $17
+	jp z, testDPad
 	inc a
 	ld [$fe02], a
-	jp pass
+	jp testDPad
 
 .b
+	ld a, 6
+	ld [new_jp], a
 	call checkControl
 	call waitVBlank
 	ld a, [$fe02]
+	cp 0
+	jp z, testDPad
 	dec a
 	ld [$fe02], a
+	jp testDPad
 
-pass:
+.start
+	call waitVBlank
+	ld a, [$fe00]; In screen pixel coords, NOT oam coords!
+	sub a, 16
+	rlca
+	rlca
+	ld h, a
+	ld a, [$fe01]; Screen pixel coords as well
+	sub a, 8
+	and $F8 ; To ensure we can use `rra`, overall faster than `srl a`
+	rra
+	rra
+	rra
+	xor h
+	and SCRN_VX_B - 1 ; $1F
+	xor h
+	ld l, a ; Got low byte of address!
+	ld a, h ; Get back vertical position
+	and $03 ; We only care about the "high byte" bits
+	or HIGH($9800) ; $98, for example. Works with any base, as long as its low byte is $00
+	ld h, a
 	
-	jp mainGame
+	ld a, [$fe02]
+	ld [hl], a
+
+testDPad:
+	ld b, 8
+	ld hl, $ff00
+	set 5, [hl]
+	res 4, [hl]
+
+.loop
+	ld a, [$ff00]
+	bit 0, a
+	jr z, .right
+	bit 1, a
+	jr z, .left
+	bit 2, a
+	jr z, .up
+	bit 3, a
+	jr z, .down
+	jp nz, testButtons
+
+.right
+	ld a, 1
+	ld [new_jp], a
+	call checkControl
+	call waitVBlank
+	ld b, 8
+	ld a, [$fe01]
+	cp 160
+	jp z, testButtons
+	add a, b
+	ld [$fe01], a
+	jp testButtons
+
+.left
+	ld a, 2
+	ld [new_jp], a
+	call checkControl
+	call waitVBlank
+	ld b, 8
+	ld a, [$fe01]
+	cp 8
+	jp z, testButtons
+	sub a, b
+	ld [$fe01], a
+	jp testButtons
+
+.up
+	ld a, 3
+	ld [new_jp], a
+	call checkControl
+	call waitVBlank
+	ld b, 16
+	ld a, [$fe00]
+	cp 16
+	jp z, testButtons
+	sub a, b
+	ld [$fe00], a
+	jp testButtons
+
+.down
+	ld a, 4
+	ld [new_jp], a
+	call checkControl
+	call waitVBlank
+	ld b, 16
+	ld a, [$fe00]
+	cp 152
+	jp z, testButtons
+	add a, b
+	ld [$fe00], a
+	jp testButtons
 
 checkControl:
 ; checks to see if previous control input is the same as the last one; if so, pass
-	ld a, [$ff00]
+	ld a, [new_jp]
 	ld b, a
-	ld a, [$c000]
+	ld a, [old_jp]
 	cp b
-	jr z, pass
+	jp z, testButtons
 	ld a, b
-	ld [$c000], a
+	ld [old_jp], a
+	xor a
+	ld [new_jp], a
+	ret
 
 wait:
-	ld a, $ff
-	ld b, $ff
-.loop
-	dec a
-	cp 0
-	jr nz, .loop
-	ld a, b
-.loop2
-	dec a
-	cp 0
-	jr nz, .loop2
 	ret
+
 enableLCD:
 	ld a, LCDCF_ON|LCDCF_BG8000|LCDCF_BGON|LCDCF_OBJ8|LCDCF_OBJON
 	ldh [rLCDC], a
